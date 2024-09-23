0e1f38a2028c07b72e842f93c345122642192814
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-09-23 22:44:48 +0700

    first push about-us-page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '480px'
+      },
+      fontSize: {
+        'xs': '12px'
+      },
+      colors:{
+        'main-blue': '#2A4189',
+        'second-blue': '#337AB7',
+        'main-blue-hover': '#1F336E',
+        'background-gray': '#E9ECEF'
+      }
+    },
+  },
+  plugins: [],
+}
+
