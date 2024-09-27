fbb108e2939d1150092f09434f9a36a5d0dcda49
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-09-27 12:54:49 +0700

    homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,19 @@ module.exports = {
         'main-blue-hover': '#1F336E',
         'background-gray': '#E9ECEF',
         'second-gray': '#F1F1F1'
-      }
+      },
+      keyframes: {
+        'border-run': {
+          '0%': { 'border-color': 'transparent transparent transparent transparent' },
+          '25%': { 'border-color': 'blue transparent transparent transparent' },
+          '50%': { 'border-color': 'blue blue transparent transparent' },
+          '75%': { 'border-color': 'blue blue blue transparent' },
+          '100%': { 'border-color': 'blue blue blue blue' },
+        },
+      },
+      animation: {
+        'border-run': 'border-run 1.5s linear forwards',
+      },
     },
   },
   plugins: [],
