3724581baf15a7ba3ff16d599626294f38f7b9ef
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-10-14 15:46:45 +0700

    edit 10

--- assets/js/scrollEventNav.js
+++ assets/js/scrollEventNav.js
@@ -1,12 +1,15 @@
 const navbarBlock = document.getElementById("navbar-block")
+const navbarBlock2 = document.getElementById("navbar-block-2")
 
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
-    const triggerPosition = 180;
+    const triggerPosition = 160;
 
     if (scrollPosition > triggerPosition) {
-        navbarBlock.classList.add('fixed');
+        navbarBlock.classList.add('opacity-0');
+        navbarBlock2.classList.add('opacity-100');
     } else {
-        navbarBlock.classList.remove('fixed');
+        navbarBlock.classList.remove('opacity-0');
+        navbarBlock2.classList.remove('opacity-100');
     }
 });
